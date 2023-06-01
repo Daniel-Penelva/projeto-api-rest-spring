773dde7b464c1d2ee72deef4d3191a62cc45a382
Daniel Penelva <d4n.andrade@gmail.com>
2023-06-01 02:11:45 -0300

    Adicionando as anotações para o funcionamento do projeto Rest Spring Boot

--- projetospringrestapi/src/main/java/projeto/api/rest/ProjetospringrestapiApplication.java
+++ projetospringrestapi/src/main/java/projeto/api/rest/ProjetospringrestapiApplication.java
@@ -1,9 +1,23 @@
 package projeto.api.rest;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"projeto.api.rest.model"})
+@ComponentScan(basePackages = {"projeto.*"})
+@EnableJpaRepositories(basePackages = {"projeto.api.rest.repository"})
+@EnableTransactionManagement
+@EnableWebMvc
+@RestController
+@EnableAutoConfiguration
 public class ProjetospringrestapiApplication {
 
 	public static void main(String[] args) {

c61ca1221daa5464912832d00f8a6b784cfdd1dc
Daniel Penelva <d4n.andrade@gmail.com>
2023-06-03 02:18:21 -0300

    Criando o controller

---
+++ projetospringrestapi/src/main/java/projeto/api/rest/controller/IndexController.java
@@ -0,0 +1,17 @@
+package projeto.api.rest.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+	
+	@GetMapping(value = "/", produces = "application/json")
+	public ResponseEntity init() {
+		
+		return new ResponseEntity("Olá Rest Spring Boot", HttpStatus.OK);
+	}
+
+}

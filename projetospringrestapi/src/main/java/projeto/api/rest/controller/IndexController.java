d8cf978e02c0ed0793ec1842d8b5c17ff5e18c23
Daniel Penelva <d4n.andrade@gmail.com>
2023-06-03 02:24:35 -0300

    Usando @RequestMapping para mapear o usuário

--- projetospringrestapi/src/main/java/projeto/api/rest/controller/IndexController.java
+++ projetospringrestapi/src/main/java/projeto/api/rest/controller/IndexController.java
@@ -3,15 +3,17 @@ package projeto.api.rest.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/usuario")
 public class IndexController {
 	
 	@GetMapping(value = "/", produces = "application/json")
 	public ResponseEntity init() {
 		
-		return new ResponseEntity("Olá Rest Spring Boot", HttpStatus.OK);
+		return new ResponseEntity("Olá usuário Rest Spring Boot", HttpStatus.OK);
 	}
 
 }

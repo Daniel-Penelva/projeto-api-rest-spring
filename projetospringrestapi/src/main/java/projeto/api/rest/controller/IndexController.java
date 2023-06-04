17739edf45b970ac8ff114e00c25dbf4b00999c4
Daniel Penelva <d4n.andrade@gmail.com>
2023-06-04 20:15:30 -0300

    Passando o parâmetro (@RequestParam) dentro da requisição.

--- projetospringrestapi/src/main/java/projeto/api/rest/controller/IndexController.java
+++ projetospringrestapi/src/main/java/projeto/api/rest/controller/IndexController.java
@@ -4,16 +4,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/* Arquitetura RestFull */
 @RestController
 @RequestMapping(value = "/usuario")
 public class IndexController {
 	
+	/* Serviço RestFull */
 	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity init() {
+	public ResponseEntity init(@RequestParam(value = "nome") String nome) {
 		
-		return new ResponseEntity("Olá usuário Rest Spring Boot", HttpStatus.OK);
+		System.out.println("Parâmetro sendo recebido " + nome);
+		
+		return new ResponseEntity("Olá usuário Rest Spring Boot,seu nome é " + nome, HttpStatus.OK);
 	}
 
 }

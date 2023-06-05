e03b39a050a6e18d470add4ead8e3a0284e9be51
Daniel Penelva <d4n.andrade@gmail.com>
2023-06-04 22:10:10 -0300

    Criando serviço RESTful de consulta de usuário por id

--- projetospringrestapi/src/main/java/projeto/api/rest/controller/IndexController.java
+++ projetospringrestapi/src/main/java/projeto/api/rest/controller/IndexController.java
@@ -2,53 +2,35 @@ package projeto.api.rest.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import projeto.api.rest.model.Usuario;
+import projeto.api.rest.repository.UsuarioRepository;
 
 /* Arquitetura RestFull */
 @RestController
 @RequestMapping(value = "/usuario")
 public class IndexController {
-	
-	List<Usuario> listaUsu = new ArrayList<Usuario>();
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 
 	/* Serviço RestFull */
-	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity init() {
-		
-		Usuario usuario = new Usuario();
-		usuario.setId(1L);
-		usuario.setLogin("d4n.andrade@gmail.com");
-		usuario.setSenha("123");
-		usuario.setNome("Daniel");
-		
-		Usuario usuario1 = new Usuario();
-		usuario1.setId(2L);
-		usuario1.setLogin("biana@gmail.com");
-		usuario1.setSenha("123");
-		usuario1.setNome("Biana");
-		
-		Usuario usuario3 = new Usuario();
-		usuario3.setId(1L);
-		usuario3.setLogin("walter@gmail.com");
-		usuario3.setSenha("123");
-		usuario3.setNome("Walter");
-		
-		listaUsu.add(usuario);
-		listaUsu.add(usuario1);
-		listaUsu.add(usuario3);	
-			
-		// return ResponseEntity.ok(listaUsu);  // http://localhost:8080/usuario/
-		
-		// ou assim: 
-		 return new ResponseEntity(listaUsu, HttpStatus.OK);
+	@GetMapping(value = "/{id}", produces = "application/json")
+	public ResponseEntity<Usuario> init(@PathVariable(value = "id") Long id) {
+
+		Optional<Usuario> usuario = usuarioRepository.findById(id);
+
+		return new ResponseEntity(usuario.get(), HttpStatus.OK);
 	}
 
 }

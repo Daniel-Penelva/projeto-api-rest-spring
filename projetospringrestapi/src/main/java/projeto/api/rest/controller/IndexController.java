64e7498e6d7fbdb1f0b317ce73621f9e4bf3e3a3
Daniel Penelva <d4n.andrade@gmail.com>
2023-06-04 21:41:09 -0300

    Retornando uma lista de usuário em JSON para a tela

--- projetospringrestapi/src/main/java/projeto/api/rest/controller/IndexController.java
+++ projetospringrestapi/src/main/java/projeto/api/rest/controller/IndexController.java
@@ -1,5 +1,8 @@
 package projeto.api.rest.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +16,8 @@ import projeto.api.rest.model.Usuario;
 @RestController
 @RequestMapping(value = "/usuario")
 public class IndexController {
+	
+	List<Usuario> listaUsu = new ArrayList<Usuario>();
 
 	/* Serviço RestFull */
 	@GetMapping(value = "/", produces = "application/json")
@@ -23,8 +28,27 @@ public class IndexController {
 		usuario.setLogin("d4n.andrade@gmail.com");
 		usuario.setSenha("123");
 		usuario.setNome("Daniel");
+		
+		Usuario usuario1 = new Usuario();
+		usuario1.setId(2L);
+		usuario1.setLogin("biana@gmail.com");
+		usuario1.setSenha("123");
+		usuario1.setNome("Biana");
+		
+		Usuario usuario3 = new Usuario();
+		usuario3.setId(1L);
+		usuario3.setLogin("walter@gmail.com");
+		usuario3.setSenha("123");
+		usuario3.setNome("Walter");
+		
+		listaUsu.add(usuario);
+		listaUsu.add(usuario1);
+		listaUsu.add(usuario3);	
 			
-		return ResponseEntity.ok(usuario);  // http://localhost:8080/usuario/ 
+		// return ResponseEntity.ok(listaUsu);  // http://localhost:8080/usuario/
+		
+		// ou assim: 
+		 return new ResponseEntity(listaUsu, HttpStatus.OK);
 	}
 
 }

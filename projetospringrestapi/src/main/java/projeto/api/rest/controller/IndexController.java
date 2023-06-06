411f84b6d206bb3ee90076407b8d0e6af6cb0ebe
Daniel Penelva <d4n.andrade@gmail.com>
2023-06-06 01:51:04 -0300

    Criando nosso serviço POST e efetuando um cadastro de usuário - utilizando Postman

--- projetospringrestapi/src/main/java/projeto/api/rest/controller/IndexController.java
+++ projetospringrestapi/src/main/java/projeto/api/rest/controller/IndexController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +27,7 @@ public class IndexController {
 	private UsuarioRepository usuarioRepository;
 
 	/* Serviço RestFull */
+	// Consultar usuário por id
 	@GetMapping(value = "/{id}", produces = "application/json")
 	public ResponseEntity<Usuario> init(@PathVariable(value = "id") Long id) {
 
@@ -32,15 +35,41 @@ public class IndexController {
 
 		return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK); // http://localhost:8080/usuario/1
 	}
-	
-	
+
 	// Consultar lista de usuários
 	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity<List<Usuario>> listaUsuario(){
-		
+	public ResponseEntity<List<Usuario>> listaUsuario() {
+
 		List<Usuario> listUsu = (List<Usuario>) usuarioRepository.findAll();
-		
+
 		return new ResponseEntity<List<Usuario>>(listUsu, HttpStatus.OK); // http://localhost:8080/usuario/
+	}
+
+	// Enviar dados de usuário
+	@PostMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario) {
+
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK); // http://localhost:8080/usuario/
+	}
+
+	// Para simular como se estivesse chamando um outro método
+	@PostMapping(value = "/vendausuario", produces = "application/json")
+	public ResponseEntity<Usuario> cadastrarVenda(@RequestBody Usuario usuario) {
+
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK); // http://localhost:8080/usuario/vendausuario/
+	}
+
+	// Para simular como se estivesse chamando um outro método
+	@PostMapping(value = "/{iduser}/idvenda/{idvenda}", produces = "application/json")
+	public ResponseEntity cadastrarVendaUsu(@PathVariable Long iduser, @PathVariable Long idVenda) {
+
+		//Usuario usuarioSalvo = usuarioRepository.save(usuario);
+
+		return new ResponseEntity("id user: " + iduser + " id venda: " + idVenda, HttpStatus.OK); // http://localhost:8080/usuario/5/idvenda/80
 	}
 
 }

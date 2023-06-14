811bf528c1902bdbe25bdf4b20fa8711698f607b
Daniel Penelva <d4n.andrade@gmail.com>
2023-06-13 23:03:03 -0300

    Cross Origin - Configuração centralizada (Global)

--- projetospringrestapi/src/main/java/projeto/api/rest/ProjetospringrestapiApplication.java
+++ projetospringrestapi/src/main/java/projeto/api/rest/ProjetospringrestapiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -31,35 +32,10 @@ public class ProjetospringrestapiApplication implements WebMvcConfigurer{
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		
-		// Libera o acesso a todos os controllers e todos os end-points (Métodos HTTP).
-		//registry.addMapping("/**");
-		
-		// Liberar todos os end-points que estão dentro do usuário, no caso, todo o /usuario que estão dentro do IndexController
-		//registry.addMapping("/usuario");
-		
-		// Liberar todos os acessos do /usuario que estão dentro do IndexController
-		//addMapping retorna um 'pathPattern' do tipo String. E nele podemos trabalhar com cabeçalho, método, origins ...
-		//allowedMethods("*") o asteristico é para liberar para todos os métodos HTTP (end-points).
-		//registry.addMapping("/usuario/**").allowedMethods("*");
-		
-		//Para os métodos que você especificar para ficar liberado, no caso, o POST e o DELETE
-		//registry.addMapping("/usuario/**").allowedMethods("POST", "DELETE");
-		
-		// Também pode dar a origins para liberar requisições POST e DELETE que vierem somente do servidor da google.com.
-		//registry.addMapping("/usuario/**").allowedMethods("POST", "DELETE").allowedOrigins("https://www.google.com/");
-		
-		// Para mais de um servidor, tb pode...
-		//registry.addMapping("/usuario/**").allowedMethods("POST", "DELETE").allowedOrigins("https://www.google.com/", "https://github.com/");
-		
-		// Para liberar o acesso para sua máquina local
-		//registry.addMapping("/usuario/**").allowedMethods("POST", "DELETE").allowedOrigins("http://8080");
-		
+		// Liderando o mapeamento de usuário para todas as origins e métodos HTTP
 		registry.addMapping("/usuario/**")
-		        .allowedOrigins("\"https://www.google.com/\"")
-		        .allowedMethods("GET", "POST")
-		        .allowedHeaders("Authorization", "Content-Type")
-		        .allowCredentials(true).maxAge(3600);
-		
+		        .allowedOrigins("*")
+		        .allowedMethods("*");
 	}
 
 }

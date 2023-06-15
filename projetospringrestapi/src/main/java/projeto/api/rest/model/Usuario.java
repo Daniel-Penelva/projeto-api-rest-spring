6ba8edb4f5defdd78032f28440e820e781392b3b
Daniel Penelva <d4n.andrade@gmail.com>
2023-06-14 22:32:26 -0300

    Spring Security - Configurando Usuário

--- projetospringrestapi/src/main/java/projeto/api/rest/model/Usuario.java
+++ projetospringrestapi/src/main/java/projeto/api/rest/model/Usuario.java
@@ -2,19 +2,29 @@ package projeto.api.rest.model;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.CascadeType;
+import javax.persistence.ConstraintMode;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.JoinTable;
 import javax.persistence.OneToMany;
+import javax.persistence.UniqueConstraint;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
-public class Usuario implements Serializable {
+public class Usuario implements UserDetails {
 
 	private static final long serialVersionUID = 1L;
 
@@ -29,8 +39,39 @@ public class Usuario implements Serializable {
 	// Declarando a Lista de Telefones
 	// Na regra um usuario pode ter nenhum ou vários telefones - tem que mapear para o usuário
 	// 'orphanRemoval = true' e 'cascade' -> para remover o usuário e pode ser em cascata
-	@OneToMany(mappedBy = "usuario", orphanRemoval = true, cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@OneToMany(mappedBy = "usuario", orphanRemoval = true, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 	private List<Telefone> telefones = new ArrayList<Telefone>();
+	
+	
+	// Autorizações ou papéis do usuário
+	@OneToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "usuarios_role", 
+	uniqueConstraints = @UniqueConstraint(
+			columnNames = {"usuario_id", "role_id"}, 
+			name = "unique_role_user"),
+	joinColumns = @JoinColumn(
+			name = "usuario_id",
+			referencedColumnName = "id",
+			table = "usuario",
+			unique = false,
+			foreignKey = @ForeignKey(
+					name = "usuario_fk", 
+					value = ConstraintMode.CONSTRAINT
+					)
+			), 
+	inverseJoinColumns = @JoinColumn(
+			name="role_id", 
+			referencedColumnName = "id", 
+			table = "role",
+			unique = false,
+			updatable = false,
+			foreignKey = @ForeignKey(
+					name = "role_fk",
+					value = ConstraintMode.CONSTRAINT
+					)
+			)
+	)
+	private List<Role> roles;
 
 	
 	// Getters e setters
@@ -92,7 +133,55 @@ public class Usuario implements Serializable {
 		Usuario other = (Usuario) obj;
 		return Objects.equals(id, other.id);
 	}
+
 	
+	// Retorna uma coleção de objetos `GrantedAuthority`, que representam as autorizações ou papéis do usuário.
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		
+		return roles;
+	}
 
+	// Retorna a senha do usuário.
+	@Override
+	public String getPassword() {
+		
+		return this.senha;
+	}
 
+	// Retorna o nome de usuário do usuário
+	@Override
+	public String getUsername() {
+		
+		return this.login;
+	}
+
+	// Verifica se a conta do usuário não está expirada.
+	@Override
+	public boolean isAccountNonExpired() {
+		
+		return true;
+	}
+
+	// Verifica se a conta do usuário não está bloqueada.
+	@Override
+	public boolean isAccountNonLocked() {
+		
+		return true;
+	}
+
+	// Verifica se as credenciais do usuário não estão expiradas.
+	@Override
+	public boolean isCredentialsNonExpired() {
+		
+		return true;
+	}
+
+	// Verifica se a conta do usuário está habilitada
+	@Override
+	public boolean isEnabled() {
+		
+		return true;
+	}
+	
 }

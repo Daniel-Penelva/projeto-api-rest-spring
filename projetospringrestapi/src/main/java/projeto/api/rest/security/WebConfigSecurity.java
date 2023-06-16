77833b1ff8e22943ff14c5d3c8d0da74614e3a19
Daniel Penelva <d4n.andrade@gmail.com>
2023-06-16 20:14:41 -0300

    Finalizando a Class WebConfigSecurity

--- projetospringrestapi/src/main/java/projeto/api/rest/security/WebConfigSecurity.java
+++ projetospringrestapi/src/main/java/projeto/api/rest/security/WebConfigSecurity.java
@@ -2,11 +2,13 @@ package projeto.api.rest.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -27,30 +29,29 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
-		/* linha 35 - Ativando a proteção contra usuário que não estão validados por token 
-		 * linha 36 - Ativando a permissão para acesso a página inicial do sistema, exemplo, "sistema.com.br/index.html" 
-		 * linha 37 - URL de logout, ele redireciona após o user deslogar o sistema 
-		 * linha 38 - Mapeia a URL de logout e invalida o usuário*/
+		/* linha 39 - Ativando a proteção contra usuário que não estão validados por token 
+		 * linha 40 - Ativando a permissão para acesso a página inicial do sistema, exemplo, "sistema.com.br/index.html" 
+		 * linha 41 - URL de logout, ele redireciona após o user deslogar o sistema 
+		 * linha 42 - Mapeia a URL de logout e invalida o usuário
+		 * linha 43 - Filtra requisições de login para autenticação
+		 * linha 44 - Filtra demais requisições para verificar a presenção do TOKEN JWT no HEADER HTTP */
 		
 		http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
 		.disable().authorizeRequests().antMatchers("/").permitAll().antMatchers("/index").permitAll()
 		.anyRequest().authenticated().and().logout().logoutSuccessUrl("/index")
-		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
-		
-		/* Filtra requisições de login para autenticação */
-		
-		
-		/* Filtra demais requisições para verificar a presenção do TOKEN JWT no HEADER HTTP */
+		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+		.and().addFilterBefore(new JWTLoginFilter("/login", authenticationManager()), UsernamePasswordAuthenticationFilter.class)
+		.addFilterBefore(new JWTApiAutenticacaoFilter(), UsernamePasswordAuthenticationFilter.class);
 		
 	}
 	
 	@Override
-		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 			
-		    /* Service que irá consultar o usuário no banco de dados. No password vamos utilizar 'BCryptPasswordEncoder()' que é um 
-		     * padrão de codificação da senha do usuário*/
+		/* Service que irá consultar o usuário no banco de dados. No password vamos utilizar 'BCryptPasswordEncoder()' que é um 
+		 * padrão de codificação da senha do usuário*/
 		
-			auth.userDetailsService(implementacaoUserDetailsService).passwordEncoder(new BCryptPasswordEncoder());
-		}
+		auth.userDetailsService(implementacaoUserDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+	}
 
 }
